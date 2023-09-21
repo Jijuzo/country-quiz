6533d1bd117bc0c80656b8a01b02faf796cb48c9
Ihor Marchuk <usdreldo@gmai.com>
2023-09-21 16:12:01 +0200

    media queries added for responsive layout

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -5,5 +5,5 @@ const errorMessage =
   "Something went wrong while downloading countries information. Please check your internet connection and refresh the page.";
 
 export const ErrorMessage = ({ error }) => {
-  return error ? <div className="error-alert">{errorMessage}</div> : null;
+  return error ? <div className="error-message">{errorMessage}</div> : null;
 };
