14f3b57a199325e15c9bd6d3cdbc17e705c48b6b
Ihor Marchuk <usdreldo@gmai.com>
2023-10-20 17:51:14 +0200

    .jsx files removed

--- src/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-import "./ErrorMessage.css";
-
-const errorMessage =
-  "Something went wrong while downloading countries information. Please check your internet connection and refresh the page.";
-
-export const ErrorMessage = ({ error }) => {
-  return error ? <div className="error-message">{errorMessage}</div> : null;
-};
