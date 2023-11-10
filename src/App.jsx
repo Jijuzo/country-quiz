446b1437be1ee35a420a30af700c8e9b04518eb3
Ihor Marchuk <usdreldo@gmai.com>
2023-11-10 10:44:13 +0100

    questionType extracted, allCountriesData added to useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { QuestionCard } from "./QuestionCard";
 import { useEffect, useState, useReducer } from "react";
 import { createRoot } from "react-dom/client";
+import { QuestionCard } from "./QuestionCard";
 import { Footer } from "./Footer";
 import { QuizResults } from "./QuizResults";
 import { DotSpinner } from "@uiball/loaders";
 import { ErrorMessage } from "./ErrorMessage";
 
 const BASE_URL = "https://restcountries.com";
-const CAPITAL_QUESTION_TYPE = 0;
-const FLAG_QUESTION_TYPE = 1;
 const QUESTION_DATA_URL = new URL(
   "/v3.1/all?fields=name,capital,flags",
   BASE_URL
@@ -16,12 +14,30 @@ const QUESTION_DATA_URL = new URL(
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "loader":
-      return { error: null, isLoading: true };
+    case "updateCountries":
+      return {
+        allCountriesData: action.updateCountries,
+        error: null,
+        isLoading: false,
+      };
+    case "loading":
+      return {
+        allCountriesData: state.allCountriesData,
+        error: null,
+        isLoading: true,
+      };
     case "error":
-      return { error: action.payload, isLoading: state.isLoading };
-    case "noLoader":
-      return { error: state.error, isLoading: false };
+      return {
+        allCountriesData: state.allCountriesData,
+        error: action.setError,
+        isLoading: state.isLoading,
+      };
+    case "notLoading":
+      return {
+        allCountriesData: state.allCountriesData,
+        error: state.error,
+        isLoading: false,
+      };
   }
 };
 
@@ -31,11 +47,6 @@ const getRandomIndex = (array) => {
 
 const getRandomCountry = (countries) => {
   return countries[getRandomIndex(countries)];
-};
-
-const getQuestionType = (min, max) => {
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-  return randomNumber;
 };
 
 const getQuestionData = (allCountriesData) => {
@@ -56,22 +67,18 @@ const getQuestionData = (allCountriesData) => {
 };
 
 export const App = () => {
-  const [allCountriesData, setAllCountriesData] = useState(null);
   const [isQuizEnded, setIsQuizEnded] = useState(false);
   const [quizScore, setQuizScore] = useState(0);
   const [answerChoices, setAnswerChoices] = useState();
   const [rightCountry, setRightCountry] = useState(null);
-  const [questionType, setQuestionType] = useState(
-    getQuestionType(CAPITAL_QUESTION_TYPE, FLAG_QUESTION_TYPE)
-  );
-  const [state, dispatch] = useReducer(reducer, {
+  const [allCountries, dispatch] = useReducer(reducer, {
+    allCountriesData: null,
     error: null,
     isLoading: false,
   });
 
   const setQuestionData = (allCountriesData) => {
     const answerChoices = getQuestionData(allCountriesData);
-    setQuestionType(getQuestionType(CAPITAL_QUESTION_TYPE, FLAG_QUESTION_TYPE));
     setRightCountry(answerChoices[getRandomIndex(answerChoices)]);
     setAnswerChoices(answerChoices);
   };
@@ -79,20 +86,20 @@ export const App = () => {
   useEffect(() => {
     if (rightCountry) return;
     const fetchQuestionData = async () => {
-      dispatch({ type: "loader" });
+      dispatch({ type: "loading" });
       try {
         const promise = await fetch(QUESTION_DATA_URL);
         if (!promise.ok) {
           throw new Error(`HTTP error! Status: ${promise.status}`);
         }
         const result = await promise.json();
-        setAllCountriesData(result);
+        dispatch({ type: "updateCountries", updateCountries: result });
         setQuestionData(result);
       } catch (error) {
         console.error("Error:", error);
-        dispatch({ type: "error", payload: error });
+        dispatch({ type: "error", setError: error });
       } finally {
-        dispatch({ type: "noLoader" });
+        dispatch({ type: "notLoading" });
       }
     };
 
@@ -103,7 +110,7 @@ export const App = () => {
 
   return (
     <main className="page">
-      {state.isLoading ? (
+      {allCountries.isLoading ? (
         <div className="spinner-container">
           <DotSpinner size={100} speed={0.9} color="black" />
         </div>
@@ -115,12 +122,11 @@ export const App = () => {
               <QuestionCard
                 rightCountry={rightCountry}
                 answerChoices={answerChoices}
-                questionType={questionType}
                 quizScore={quizScore}
                 onCorrectAnswer={setQuizScore}
                 isQuizEnded={isQuizEnded}
                 onIncorrectAnswer={setIsQuizEnded}
-                allCountriesData={allCountriesData}
+                allCountriesData={allCountries.allCountriesData}
                 setNextQuestion={setQuestionData}
               />
             )}
@@ -130,13 +136,13 @@ export const App = () => {
                 onTryAgain={setQuestionData}
                 quizScore={quizScore}
                 setQuizScore={setQuizScore}
-                allCountriesData={allCountriesData}
+                allCountriesData={allCountries.allCountriesData}
               />
             )}
           </form>
         </div>
       )}
-      <ErrorMessage error={state.error} />
+      <ErrorMessage error={allCountries.error} />
       <Footer />
     </main>
   );
