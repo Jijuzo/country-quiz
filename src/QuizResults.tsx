786787df546129c05e162a2269cbcaa0eb0ae5ab
Ihor Marchuk <usdreldo@gmai.com>
2023-11-22 21:51:20 +0100

    props reduced

--- src/QuizResults.tsx
+++ src/QuizResults.tsx
@@ -2,35 +2,20 @@ import cardWinnerIconSvg from "./assets/undraw_winners_ao2o_2.svg";
 import "./QuizResults.css";
 
 type QuizResultsProps = {
-  setIsQuizEnded: React.Dispatch<React.SetStateAction<boolean>>;
+  onTryAgain: () => void;
   quizScore: number;
-  setQuizScore: React.Dispatch<React.SetStateAction<number>>;
 };
 
-export function QuizResults({
-  setIsQuizEnded,
-  quizScore,
-  setQuizScore,
-}: QuizResultsProps) {
+export function QuizResults({ quizScore, onTryAgain }: QuizResultsProps) {
   return (
     <div className="quiz-results-div">
-      <img
-        className="card-winner-icon"
-        src={cardWinnerIconSvg}
-        alt="winner icon"
-      />
+      <img className="card-winner-icon" src={cardWinnerIconSvg} alt="" />
       <p className="quiz-results-text">Results</p>
       <p className="correct-answers-content">
         You got <span className="correct-answers-quantity">{quizScore}</span>{" "}
         correct answers
       </p>
-      <button
-        className="try-again-button"
-        onClick={() => {
-          setIsQuizEnded(false);
-          setQuizScore(0);
-        }}
-      >
+      <button className="try-again-button" type="submit" onClick={onTryAgain}>
         try again
       </button>
     </div>
