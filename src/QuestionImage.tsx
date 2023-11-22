23bce7911bf46e3deb70a10515232553d512fb21
Ihor Marchuk <usdreldo@gmai.com>
2023-11-22 21:54:25 +0100

    prop name changed

--- src/QuestionImage.tsx
+++ src/QuestionImage.tsx
@@ -1,14 +1,10 @@
-import { AllCountriesType } from "./App";
+import { AllCountries } from "./App";
 
-export function QuestionImage({
-  rightCountry,
-}: {
-  rightCountry: AllCountriesType[number];
-}) {
+export function QuestionImage({ country }: { country: AllCountries[number] }) {
   return (
     <img
       className="question-flag"
-      src={`${rightCountry.flags.png}`}
+      src={`${country.flags.png}`}
       width="84px"
       height="54px"
       style={{ border: "1px solid black" }}
